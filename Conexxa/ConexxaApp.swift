68c7e46053ce91d0abe87a92b1843d6a805d5585
Gabriel Castillo <sk8.andrade@gmail.com>
2023-09-10 19:39:26 +0200

    Initial Commit

---
+++ Conexxa/ConexxaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConexxaApp.swift
+//  Conexxa
+//
+//  Created by Gabriel Castillo Serafim on 10/9/23.
+//
+
+import SwiftUI
+
+@main
+struct ConexxaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
